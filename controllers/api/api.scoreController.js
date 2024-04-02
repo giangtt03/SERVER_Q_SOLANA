f2e2622ef931a0ef53d788f9875864b48fa68b7c
giangtt03 <giangtt653@gmail.com>
2024-04-02 15:15:34 +0700

    score

--- controllers/api/api.scoreController.js
+++ controllers/api/api.scoreController.js
@@ -1,20 +1,42 @@
+// Import các model cần thiết
 const Session = require('../../models/api/Session');
 const UserScore = require('../../models/api/UserScore');
 
 module.exports = {
-    // Lấy ra tổng điểm của người dùng từ các bài kiểm tra đã làm
     getUserTotalScore: async (userId) => {
         try {
+            let totalScoreFromSessions = 0;
             const userSessions = await Session.find({ userId: userId });
-
-            // Tính tổng điểm từ các phiên làm bài
-            let totalScore = 0;
             userSessions.forEach(session => {
-                totalScore += session.score;
+                totalScoreFromSessions += session.score;
             });
+
+            const userScore = await UserScore.findOne({ userId });
+            const totalScoreFromUserScore = userScore ? userScore.totalScore : 0;
+
+            const totalScore = totalScoreFromSessions + totalScoreFromUserScore;
+
             return totalScore;
         } catch (error) {
             console.error("Error getting user total score:", error);
+            throw error;
+        }
+    },
+
+    updateUserScoreAfterNFTExchange: async (userId, scoreToSubtract) => {
+        try {
+            let userScore = await UserScore.findOne({ userId });
+            if (!userScore) {
+                userScore = new UserScore({
+                    userId: userId,
+                    totalScore: 0
+                });
+            }
+
+            userScore.totalScore -= scoreToSubtract;
+            await userScore.save();
+        } catch (error) {
+            console.error("Error updating user score after NFT exchange:", error);
             throw error;
         }
     }
