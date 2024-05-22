d5feca258143f4dff7378f0f6484ff405b0d1848
giangtt03 <giangtt653@gmail.com>
2024-05-22 22:00:29 +0700

    up 1 vài thứ trong models

--- models/api/User.js
+++ models/api/User.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
-    unique: true,
-  },
+    required: true
+    },
   email: {
     type: String,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   avatar: {
     type: String,
-    default: ''
+    required: true
+
   },
   solanaAddress: {
     type: String,
-    default: ''
-  },
+    required: true
+  }
 });
 
 module.exports = mongoose.model('TKNguoiDung', UserSchema);
